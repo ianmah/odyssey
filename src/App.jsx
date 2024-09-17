e7d7c3fc04c297f003c9e488dde3d57c46899517
ian mah <ianmmah@gmail.com>
2024-09-17 05:26:28 +0000

    new menu and new responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,97 @@
-import { useState } from 'react'
-import {usePrivy} from '@privy-io/react-auth'
-import running from '/running.gif'
-import bg3 from '/bg3.webp'
-import styled from 'styled-components'
-import Modal from './components/Modal'
-import './App.css'
+import { useState } from "react";
+import { usePrivy } from "@privy-io/react-auth";
+import running from "/running.gif";
+import bg4 from "/bg4.png";
+import styled from "styled-components";
+import Modal from "./components/Modal";
+import "./App.css";
+import Button from "./components/Button";
 
 const Main = styled.main`
-  background:url(${bg3});
+  background: url(${bg4});
   background-size: cover;
-  padding: 1.5em;
+  padding: 1em;
   min-height: 100vh;
   box-sizing: border-box;
-`
+  width: 46vh;
+`;
 
 const Img = styled.img`
   filter: drop-shadow(0px 7px 5px #22222255);
-`
+`;
+
+const Card = styled.div`
+  padding: 1em;
+  width: 280px;
+  background: white;
+  border-radius: 1em;
+  margin: 1em;
+`;
+
+const ButtonMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  position: absolute;
+  width: 44vh;
+  bottom: 1em;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 function App() {
-  const {ready, authenticated, login, user} = usePrivy();
+  const { ready, authenticated, login, user } = usePrivy();
   // Disable login when Privy is not ready or the user is already authenticated
   const disableLogin = !ready || (ready && authenticated);
   const [questModal, setQuestModal] = useState(false);
 
-  console.log(user)
-
   return (
     <Main>
-      {questModal && <Modal onExit={() => setQuestModal(false)}>
-        <h1>Quests</h1>
+      {questModal && (
+        <Modal onExit={() => setQuestModal(false)}>
+          <h1>Quests</h1>
+          <Card>Quest 1</Card>
+          <Card>Quest 2</Card>
+          <Card>Quest 3</Card>
         </Modal>
-      }
-    <button disabled={disableLogin} onClick={login}>
-      Log in
+      )}
+      <button disabled={disableLogin} onClick={login}>
+        Log in
       </button>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div className="card">
-        <Img src={running} className="char" width="200" alt="Character sprite" /> 
+        <Img
+          src={running}
+          className="char"
+          width="200"
+          alt="Character sprite"
+        />
       </div>
-      <button onClick={() => setQuestModal(true)}>
-        Quest
-      </button>
+      <ButtonMenu>
+        <Button>Bag</Button>
+        <Button>Kitchen</Button>
+        <Button>Quest</Button>
+        <Button>Map</Button>
+        <Button>Rank</Button>
+      </ButtonMenu>
     </Main>
-  )
+  );
 }
 
-export default App
+export default App;
