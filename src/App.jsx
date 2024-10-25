34dfdc29493092429651204113dcdfa97ff18333
unknown <ianmmah@gmail.com>
2024-10-25 00:58:33 -0700

    page selector

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import styled from "styled-components";
 import Login from "./pages/Login";
 
 const Main = styled.main`
-  background: url(${p => p.authenticated ? bg4 : bg1});
+  background: url(${p => p.authenticated ? p.bg : bg1});
   background-size: cover;
   padding: 1em;
   min-height: 100vh;
@@ -41,40 +41,68 @@ const ButtonMenu = styled.div`
 
 function App() {
   const { authenticated, login, user, logout } = usePrivy();
+  const [currentPage, setCurrentPage] = useState('main');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'main':
+        return (
+          <>
+            <CharacterMenu />
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <button onClick={logout}>
+                Log Out
+              </button>
+            </div>
+            <div style={{ marginTop: '50vh', textAlign: 'center', lineHeight: '1em' }}>
+              <Img
+                src={char4x}
+                className="char"
+                width="64"
+                alt="Character sprite"
+              />
+              <span style={{
+                display: 'block', 
+                color: 'white', 
+                textShadow: '0px 0px 6px black, 0px 0px 6px black, 0px 0px 6px black, 0px 0px 6px black',
+              }}>
+                SloshJosh
+              </span>
+            </div>
+          </>
+        );
+      case 'bag':
+        return <div>Bag Page</div>;
+      case 'dojo':
+        return <div>Dojo Page</div>;
+      default:
+        return null;
+    }
+  };
+  const getBackgroundImage = () => {
+    switch (currentPage) {
+      case 'main':
+        return bg4;
+      case 'bag':
+        return bg4;
+      case 'dojo':
+        return 'url("/bg-dojo.png")';
+      default:
+        return 'url("/bg-default.png")';
+    }
+  };
   
   return (
-  <Main authenticated={authenticated}>
+  <Main authenticated={authenticated} bg={getBackgroundImage()}>
   {!authenticated && <Login/>}
+  {authenticated && renderPage()}
   {authenticated && (
-    <>
-      <CharacterMenu />
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <button onClick={logout}>
-          Log Out
-        </button>
-      </div>
-      <div style={{ marginTop: '50vh', textAlign: 'center', lineHeight: '1em' }}>
-        <Img
-          src={char4x}
-          className="char"
-          width="64"
-          alt="Character sprite"
-        />
-        <span style={{
-          display: 'block', 
-          color: 'white', 
-          textShadow: '0px 0px 6px black, 0px 0px 6px black, 0px 0px 6px black, 0px 0px 6px black',
-        }}>
-          SloshJosh
-        </span>
-      </div>
-      <ButtonMenu>
-        <Button>Bag</Button>
-        <Button>Dojo</Button>
-        <Button>Map</Button>
-      </ButtonMenu>
-      </>
-    )}
+    <ButtonMenu>
+      <Button onClick={() => setCurrentPage('bag')}>Bag</Button>
+      <Button onClick={() => setCurrentPage('dojo')}>Dojo</Button>
+      <Button onClick={() => setCurrentPage('main')}>Map</Button>
+    </ButtonMenu>
+  )}
   </Main>
   )
 }
